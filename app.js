7a4cb79c5b802458eaf085d19bd6b3971c2a135b
oiik <kisson6@gmail.com>
2021-10-05 18:37:10 +0800

    autoscript

---
+++ app.js
@@ -0,0 +1,12 @@
+VANTA.TOPOLOGY({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x724d2d,
+  backgroundColor: 0xf1616
+})
